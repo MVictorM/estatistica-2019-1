e737bf8806f4a3665d2c490558c951cf09f98680
Victor Miranda <victor.miranda@idealizza.com.br>
2019-06-06 20:57:46 -0300

    q7

--- projeto.R
+++ projeto.R
@@ -38,20 +38,31 @@ print(median(gotdf[["Audiencia.Em.milhoes."]]))
 
 #4-Faça uma função que retorna apenas os nomes dos episódios que possuem notas
 #maiores ou iguais a nove (9).
-notasmaiornove = gotdf[gotdf$Nota >= 9,]["Nota"]
+notasmaiornove = gotdf[gotdf$Nota >= 9,]["Episodio"]
 print(notasmaiornove)
 
 #5-Faça uma função que retorna o nome dos episódios com menor e maior notas,
-#nessa ordem para cada uma das temporadas. Por fim, faça um dataframe com cada episódio encontrado com as colunas TÍTULO, NOTA, TEMPORADA ordenados de forma crescente por temporada
+#nessa ordem para cada uma das temporadas. Por fim, faça um dataframe com cada episódio encontrado com as colunas 
+#TÍTULO, NOTA, TEMPORADA ordenados de forma crescente por temporada
 #(de 1 até 8).
 
 
 #6-Faça uma função que retorne qual a temporada com o menor desvio padrão na
 #audiência.
+
+
 #7-Faça uma função que retorne a  média das notas dos episódios em que Brienne
 #of Tarth(Gwendoline Christie) participa.
+
+#filtra se contem Brienne e calcula a media
+print(mean(gotdf[grepl("Brienne", gotdf$Personagens),][["Nota"]]))
+print(notasBrienne)
+
 #8-Faça uma função que retorne uma lista com os personagens que só apareceram 
 #em um único episódio na quarta (4ª) temporada.
+
+
 #9-Faça uma função que dado o nome de um personagem, cria um histograma onde 
-#mostra a frequência de aparição desse personagem a cada temporada. Não esqueça de dar um título e fazer ele de forma colorida, facilitando a visualização. Um exemplo para o 
+#mostra a frequência de aparição desse personagem a cada temporada. Não esqueça de dar um título e fazer ele 
+#de forma colorida, facilitando a visualização. Um exemplo para o 
 #personagem Bran Stark(Isaac Hempstead) seria:
