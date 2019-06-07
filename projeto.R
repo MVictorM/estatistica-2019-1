5ed765c5e69a37ab981e4756afbd8eb719052720
Victor Miranda <victor.miranda@idealizza.com.br>
2019-06-07 00:49:56 -0300

    function

--- projeto.R
+++ projeto.R
@@ -38,33 +38,47 @@ print(median(gotdf[["Audiencia.Em.milhoes."]]))
 
 ######4-Faça uma função que retorna apenas os nomes dos episódios que possuem notas
 #maiores ou iguais a nove (9).
-notasmaiornove = gotdf[gotdf$Nota >= 9,]["Episodio"]
-print(notasmaiornove)
+notasmaiornove <- function() {
+  return (gotdf[gotdf$Nota >= 9,]["Episodio"])
+}
+print(notasmaiornove())
 
 ######5-Faça uma função que retorna o nome dos episódios com menor e maior notas,
 #nessa ordem para cada uma das temporadas. Por fim, faça um dataframe com cada episódio encontrado com as colunas 
 #TÍTULO, NOTA, TEMPORADA ordenados de forma crescente por temporada
 #(de 1 até 8).
+menoremaior <- function() {
 
+}
 
 ######6-Faça uma função que retorne qual a temporada com o menor desvio padrão na
 #audiência.
-
-desviosTemporadas = aggregate(Audiencia.Em.milhoes. ~ Temporada, gotdf, sd)
-print(head(desviosTemporadas[order(desviosTemporadas$Audiencia.Em.milhoes.),], 1))
+menordesvio <- function() {
+  desviosTemporadas = aggregate(Audiencia.Em.milhoes. ~ Temporada, gotdf, sd)
+  return (head(desviosTemporadas[order(desviosTemporadas$Audiencia.Em.milhoes.),], 1))
+}
+print(menordesvio())
 
 
 ######7-Faça uma função que retorne a  média das notas dos episódios em que Brienne
 #of Tarth(Gwendoline Christie) participa.
 
 #filtra se contem Brienne e calcula a media
-print(mean(gotdf[grepl("Brienne", gotdf$Personagens),][["Nota"]]))
+mediaBrienne <- function() {
+  return (mean(gotdf[grepl("Brienne", gotdf$Personagens),][["Nota"]]))
+}
+print(mediaBrienne())
 
 ######8-Faça uma função que retorne uma lista com os personagens que só apareceram 
 #em um único episódio na quarta (4ª) temporada.
+unicoquarta <- function() {
 
+}
 
 ######9-Faça uma função que dado o nome de um personagem, cria um histograma onde 
 #mostra a frequência de aparição desse personagem a cada temporada. Não esqueça de dar um título e fazer ele 
 #de forma colorida, facilitando a visualização. Um exemplo para o 
 #personagem Bran Stark(Isaac Hempstead) seria:
+histograma<- function(personagem) {
+  
+}
